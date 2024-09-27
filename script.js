84eb8d24c568f6e4532c4244be1449cdfc455d65
Hareesh123NH <162862005+Hareesh123NH@users.noreply.github.com>
2024-09-27 21:45:55 +0530

    created a submission

--- script.js
+++ script.js
@@ -4,10 +4,12 @@ const output=document.getElementById("output");
 
 btn.addEventListener("click",async()=>{
 	output.innerText="";
-	const input=document.getElementById("text").value;
-	const delay=document.getElementById("delay").value;
-	await delayFn(delay);
-	output.innerText=input;
+	let input=document.getElementById("text");
+	let delay=document.getElementById("delay");
+	await delayFn(delay.value);
+	output.innerText=input.value;
+	input.value="";
+	delay.value=0;
 });
 function delayFn(d){
 	return new Promise((res,rej)=>{
